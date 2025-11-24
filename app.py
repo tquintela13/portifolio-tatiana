355246740fc5150d0395090aa0418e2f481ba61b
Tatiana <97659635+tquintela13@users.noreply.github.com>
2025-11-23 22:25:06 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello World da Tatiana!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
